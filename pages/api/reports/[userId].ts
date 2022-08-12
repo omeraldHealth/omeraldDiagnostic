2cc29272eb3f185648517e9b4a7f4924b3a453d5
adityaKrModak <aditya.k@proximity.tech>
2022-08-12 17:08:47 +0530

    fix: session issues

--- pages/api/reports/[userId].ts
+++ pages/api/reports/[userId].ts
@@ -6,6 +6,7 @@ import { randomUUID } from "crypto";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   let { client, db } = await connectToDatabase();
+  const session = client.startSession();
   const { userId } = req.query;
   if (req.method === "GET") {
     try {
@@ -31,7 +32,6 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   }
 
   if (req.method === "POST") {
-    const session = client.startSession();
     const reportDetails = req.body as ReportDetails;
 
     try {
