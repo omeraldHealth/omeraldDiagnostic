5b96b1435fe5f360e17cff01e5ff66f7267ee512
Mohammed saif <101236642+msaif1996@users.noreply.github.com>
2023-01-14 00:59:22 +0530

    dashboard completed

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,17 +1,27 @@
 
-import { bannerDashboard } from "@/components/core/images/image";
-import ReportSharedVsTime2 from "@/components/Graphs/ReportSharedVsTime2";
+import { bannerDashboard, doctorAvatar } from "@/components/core/images/image";
+import {CategoryScale} from 'chart.js';
 import { useAuth } from "@/lib/auth";
 import { BeakerIcon, ChartBarIcon, ShareIcon } from "@heroicons/react/20/solid";
 import React from "react";
-import {CategoryScale} from 'chart.js';
+import ReportSharedVsTime2 from "@/components/Graphs/ReportSharedVsTime2";
 import Chart from 'chart.js/auto';
+import Link from "next/link";
 
 const Dashboard = () => {
 
   const {diagnosticDetails, signOut } = useAuth();
   Chart.register(CategoryScale);
 
+  const activities = [
+    {
+      action: "uploaded",
+      reporter: "Mohammed Saif",
+      time:"3 days ago",
+      reportName:"Blood Report",
+      profile: doctorAvatar
+    }
+  ]
 
   return <div className="p-8">
     <section className="relative ">
@@ -22,13 +32,16 @@ const Dashboard = () => {
        </p>
     </section>
     <section className="my-6 flex justify-between" >
-        <section className="w-[20%] h-[14vh] p-4 flex justify-between bg-blue-900 rounded-md text-white">
-            <BeakerIcon className="w-10" />
-            <span className="mt-4">
-              <p className="font-light text-sm">Tests Offered</p>
-              <p className="font-bold text-2xl flex justify-end my-2">10</p>
-            </span>
-        </section>
+        <Link href={"/test"}  passHref={true}>
+          <section className="w-[20%] h-[14vh] p-4 flex justify-between bg-blue-900 rounded-md text-white">
+              <BeakerIcon className="w-10" />
+              <span className="mt-4">
+                <p className="font-light text-sm">Tests Offered</p>
+                <p className="font-bold text-2xl flex justify-end my-2">10</p>
+              </span>
+          </section>
+        </Link>
+        <Link href={"/reports"}  passHref={true}>
         <section className="w-[20%] h-[14vh] p-4 flex justify-between bg-indigo-900 rounded-md text-white">
             <ChartBarIcon className="w-10" />
             <span className="mt-4">
@@ -36,6 +49,8 @@ const Dashboard = () => {
               <p className="font-bold text-2xl flex justify-end my-2">10</p>
             </span>
         </section>
+        </Link>
+        <Link href={"/reports"}  passHref={true}>
         <section className="w-[20%] h-[14vh] p-4 flex justify-between bg-gray-500 rounded-md text-white">
             <ShareIcon className="w-10" />
             <span className="mt-4">
@@ -43,6 +58,8 @@ const Dashboard = () => {
               <p className="font-bold text-2xl flex justify-end my-2">8</p>
             </span>
         </section>
+        </Link>
+        <Link href={"/profile"}  passHref={true}>
         <section className="w-[20%] h-[14vh] p-4 flex justify-between bg-green-900 rounded-md text-white">
             <BeakerIcon className="w-10" />
             <span className="mt-4">
@@ -50,10 +67,32 @@ const Dashboard = () => {
               <p className="font-bold text-2xl flex justify-end my-2">10 / 5</p>
             </span>
         </section>
+        </Link>
 
     </section>
-    <section className="w-[60%] bg-white">
-      <ReportSharedVsTime2 />
+    <section className="flex justify-between h-[45vh]">
+      <section className="w-[60%] h-auto bg-white">
+        <ReportSharedVsTime2 />
+      </section>
+      <section className="w-[30%] h-[100%] bg-white rounded-sm p-4">
+        <p>Recent Activities</p>
+        <p className="text-xs my-2 text-gray-400 font-light">Summary of the latest updated activities</p>
+        {
+          activities.slice(0,5).map(activity => {
+            return ( 
+              <section className="my-4 flex justify-between">
+                  <span className="text-xs flex">
+                    <img src={activity.profile} alt="user-avatar" className="w-10 rounded-full mr-4" />
+                    <span>
+                    <p className="text-light text-gray-600 mt-1">{activity.reporter} <span>{activity.action} report</span> <span className="text-orange-500">"{activity.reportName}"</span></p>
+                    <p className="text-light text-indigo-600 mt-1">{activity.time}</p>
+                    </span>
+                  </span>
+              </section>
+            )
+          })
+        }
+      </section>
     </section>
   </div>
 };;
