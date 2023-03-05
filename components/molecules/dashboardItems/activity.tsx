e0584ee0f62ccd2cdd1db99c4be460501fd0e709
Mohammed saif <101236642+msaif1996@users.noreply.github.com>
2023-03-05 14:30:15 +0530

    activity list

--- components/molecules/dashboardItems/activity.tsx
+++ components/molecules/dashboardItems/activity.tsx
@@ -1,33 +1,22 @@
 import { UserCircleIcon } from '@heroicons/react/20/solid'
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import moment from 'moment';
+import { SET_DASHBOARD_ROUTE } from 'utils/store/types';
 
 export const DashActivity = () => {
- 
-  const diagnosticDetails = useSelector((state:any)=>state.diagnosticReducer)
-  const date = new Date();
 
-  return (<section className="sm:w-[30%] h-[100%] bg-white rounded-sm p-4 mb-10 sm:mb-0 max-h-[50vh] overflow-auto">
+  const diagnosticDetails = useSelector((state:any)=>state.diagnosticReducer)
+  let activities = diagnosticDetails?.activities.sort((a:any,b:any)=>new Date(b.updatedTime) - new Date(a.updatedTime)).slice(0,5)
+
+  return (
+  <section className="sm:w-[30%] h-[100%] bg-white rounded-sm px-4 py-2 mb-10 sm:mb-0 max-h-[50vh] overflow-auto">
         <p>Recent Activities</p>
-        <p className="text-xs my-2 text-gray-400 font-light">Summary of the latest updated activities</p>
+        <p className="text-xs text-gray-400 font-light">Summary of the latest updated activities</p>
             <section>
-           {diagnosticDetails?.activities ? diagnosticDetails?.activities?.map((activity:any,index:any) => {
-                return ( 
-                <section key={index} className="my-4 flex justify-between ">
-                    <span className="text-xs flex">
-                        <UserCircleIcon className="w-8 h-8 self-center bg-blue-700 text-white rounded-full mr-2" />
-                        <span>
-                        <p className="text-light text-gray-600 mt-1"><span className='font-bold'>{activity.user.managerName} </span>{activity.activity}</p>
-                            {activity?.updatedTime ? 
-                                <span>{moment.utc(DateConverter(activity.updatedTime)).local().startOf('seconds').fromNow()}</span>:
-                                <span>{moment.utc(DateConverter(date.toISOString())).local().startOf('seconds').fromNow()}</span>
-                            }
-                        </span>
-                    </span> 
-                </section>
-                )
-            }):<p className="text-light text-sm text-gray-600 mt-8">No Activities....</p>}
+                {activities.length>0 ? 
+                <ActivityItem activityList={activities} /> :
+                <p className="text-light text-sm text-gray-600 mt-8">No Activities....</p>}
             </section> 
     </section>
 )}
@@ -36,3 +25,27 @@ const DateConverter = (dateString:any) => {
     const date = new Date(dateString);
     return date
 }  
+
+const ActivityItem = ({activityList}:any) => {
+    const dispatch = useDispatch()
+    const date = Date.now()
+    return <section>
+       {activityList.map((activity:any,index:any)=>{
+        return (
+            <section key={index} className="my-3 flex justify-between ">
+                <span className="text-xs flex">
+                    <UserCircleIcon className="w-8 h-8 self-center bg-blue-700 text-white rounded-full mr-2" />
+                    <span>
+                    <p className="text-light text-gray-600 mt-1"><span className='font-bold'>{activity.user.managerName} </span>{activity.activity}</p>
+                        {activity?.updatedTime ? 
+                            <span>{moment.utc(DateConverter(activity.updatedTime)).local().startOf('seconds').fromNow()}</span>:
+                            <span>{moment.utc(DateConverter(date.toString())).local().startOf('seconds').fromNow()}</span>
+                        }
+                    </span>
+                </span> 
+            </section>
+        )
+        })}
+    <a href="#" onClick={()=>{dispatch({type:SET_DASHBOARD_ROUTE,payload:{name:"Settings",href:"/settings"}})}} className='font-light text-xs text-blue-700'>Read More....</a>
+    </section>
+}
