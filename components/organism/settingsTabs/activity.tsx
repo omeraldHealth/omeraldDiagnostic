03a07fa1ec3c8297c01784daf9d5a4ade21fdb5c
Mohammed saif <101236642+msaif1996@users.noreply.github.com>
2023-03-07 15:46:49 +0530

    filter

--- components/organism/settingsTabs/activity.tsx
+++ components/organism/settingsTabs/activity.tsx
@@ -1,26 +1,43 @@
 import { DashboardTable } from "@components/molecules/dashboardItems/data-table";
+import moment from "moment";
+import { useState } from "react";
 import { useSelector } from "react-redux";
 
 export function Activity() {
+   
     const diagnosticDetails = useSelector((state:any)=>state.diagnosticReducer)
-    const columns = [
+    let managersList = []
+    const managers = diagnosticDetails?.managersDetail.forEach((man:any) => {
+        const obj = { 
+          text: man.managerName, 
+          value: man.managerName 
+        };
+        managersList.push(obj)
+      });
+    const columns = [   
         {
             key: 'sdas',
             title: 'Activity',
             dataIndex: 'activity',
             render: (text:any) => <a className='text-blue-800 font-medium'>{text}</a>,
+            sorter: (a:any, b:any) => a.activity.length - b.activity.length,
           },
           {
-              key: 'asse',
+              key: 'user',
               title: 'Activity By',
               dataIndex: 'user',
               render: (text:any) => <a>{text.managerName}</a>,
+              sorter: (a:any, b:any) => a.user.length - b.user.length,
+              filters: managersList,
+              onFilter: (value: string, record) => record.user.managerName.indexOf(value) === 0,
           },
           {
               key: 'qws',
               title: 'Activity Time',
               dataIndex: 'updatedTime',
-              render: (text:any) => <a>{text}</a>,
+              render: (text:any) => <a>{moment(text).format("DD/mm/yyyy HH:mm:ss")}</a>,
+              sorter: (a:any, b:any) => new Date(a.updatedTime) - new Date(b.updatedTime),
+         
           },
     ]
 
@@ -30,4 +47,5 @@ export function Activity() {
         </div>
     )
 }
+
 
