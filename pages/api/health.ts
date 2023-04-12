a4e9cfb3af237b19177998591c0b9ed4dd39d4b8
Mohammed saif <saifmohammed888@gmail.com>
2023-04-12 17:07:19 +0530

    health

--- pages/api/health.ts
+++ pages/api/health.ts
@@ -2,6 +2,10 @@ import { NextApiRequest, NextApiResponse, NextApiHandler } from 'next';
 import { send } from 'micro';
 
 const handler: NextApiHandler = async (req: NextApiRequest, res: NextApiResponse<void>) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "*");
+  res.setHeader("Access-Control-Allow-Headers", "*");
+  res.setHeader('Content-Security-Policy', 'unsafe-url');
     // Your health check logic goes here
     try {
         // Your health check logic goes here
