a658f7be4c02746067d6e2842b44d355191a4ebc
Mohammed saif <saifmohammed888@gmail.com>
2023-04-15 07:54:52 +0530

    cors

--- utils/urls/app.ts
+++ utils/urls/app.ts
@@ -1,25 +1,25 @@
 // export const backendApiPath= "https://parser-api.onrender.com"
 // export const backendApiPath = "http://localhost:3000"
-export const backendApiPath = ""
+export const backendApiPath = "https://diagnostic-omerald-dev.vercel.app"
 
 //Api paths for backend calls
-export const getDiagnosticUserApi = backendApiPath+`api/diagnostic/users/fetchByPhone?phoneNumber=`
-export const insertDiagnosticUserApi = backendApiPath+`api/diagnostic/users/insert`
-export const updateDiagnosticUserApi = backendApiPath+`api/diagnostic/users/update?phoneNumber=`
+export const getDiagnosticUserApi = backendApiPath+`/api/diagnostic/users/fetchByPhone?phoneNumber=`
+export const insertDiagnosticUserApi = backendApiPath+`/api/diagnostic/users/insert`
+export const updateDiagnosticUserApi = backendApiPath+`/api/diagnostic/users/update?phoneNumber=`
 
-// export const uploadImageApi = backendApiPath+`api/diagnostic/s3bucket/addBrandingImages`
+// export const uploadImageApi = backendApiPath+`/api/diagnostic/s3bucket/addBrandingImages`
 export const uploadImageApi = "https://parser-api.onrender.com/api/diagnostic/uploadBranding"
 
-export const getDiagnosticReports = backendApiPath+`api/diagnostic/reports/fetchByPhone?phoneNumber=`
-export const insertReportApi = backendApiPath+`api/diagnostic/reports/insert`
+export const getDiagnosticReports = backendApiPath+`/api/diagnostic/reports/fetchByPhone?phoneNumber=`
+export const insertReportApi = backendApiPath+`/api/diagnostic/reports/insert`
 
 export const uploadReportApi = "https://parser-api.onrender.com/api/diagnostic/uploadReport"  
 
-export const getEmployees = backendApiPath+`api/diagnostic/employees/fetchAll`
-export const getEmployeeById = backendApiPath+`api/diagnostic/employees/fetchByPhone?phoneNumber=`
-export const insertEmployee = backendApiPath+`api/diagnostic/employees/insert`
-export const updateEmployee = backendApiPath+`api/diagnostic/employees/update?userId=`
-export const deleteEmployee = backendApiPath+`api/diagnostic/employees/delete?phoneNumber=`
+export const getEmployees = backendApiPath+`/api/diagnostic/employees/fetchAll`
+export const getEmployeeById = backendApiPath+`/api/diagnostic/employees/fetchByPhone?phoneNumber=`
+export const insertEmployee = backendApiPath+`/api/diagnostic/employees/insert`
+export const updateEmployee = backendApiPath+`/api/diagnostic/employees/update?userId=`
+export const deleteEmployee = backendApiPath+`/api/diagnostic/employees/delete?phoneNumber=`
 
 // Omerald admin report types
 
