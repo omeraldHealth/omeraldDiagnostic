e40b763227478502bfdcae57ed5370464d566903
msaif001 <saifmohammed888@gmail.com>
2024-02-19 17:01:47 +0530

    view Test

--- components/molecules/test/testTable.tsx
+++ components/molecules/test/testTable.tsx
@@ -1,184 +1,273 @@
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { Modal, Input, Space, Tag, Popover } from 'antd';
+import { PencilIcon, TrashIcon } from '@heroicons/react/20/solid';
+import { ColumnsType } from 'antd/es/table';
+import { useUser } from '@clerk/clerk-react';
+import { useUpdateDiagnostic } from 'utils/reactQuery';
+import { useQueryClient } from 'react-query';
+import { DataType, SET_TEST } from 'utils/store/types';
+import { useCurrentBranchValue, useManagerValue, useProfileValue } from '@components/common/constants/constants';
 import { successAlert } from '@components/atoms/alerts/alert';
 import { BodyStyled_2 } from '@components/atoms/font/font.style';
-import { PencilIcon, TrashIcon } from '@heroicons/react/20/solid';
-import { Input, Modal, Popover, Space, Tag } from 'antd';
-import { ColumnsType } from 'antd/es/table';
-import React, { useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux';
-import { useAuthContext } from 'utils/context/auth.context';
-import { updateUserDetails } from 'utils/hook/userDetail';
-import { DataType, SET_TEST } from 'utils/store/types';
-import { DashboardTable } from '../dashboardItems/data-table'
+import { DashboardTable } from '../dashboardItems/data-table';
 import { AddKeywords } from '../addTest/addKeywords';
-import { useQueryGetData, useUpdateDiagnostic } from 'utils/reactQuery';
-import { useQueryClient } from 'react-query';
-import { ActivityLogger } from '../logger.tsx/activity';
-import { profileState } from '../../common/recoil/profile';
-import { useRecoilValue } from 'recoil';
-import { operatorState } from '../../common/recoil/operator';
-import { branchState } from '../../common/recoil/blogs/branch';
-import { useUser } from '@clerk/clerk-react';
-import { useCurrentBranchValue, useManagerValue, useProfileValue } from '@components/common/constants/constants';
+
+const handleRemoveTest = async (record:any) => {
+  //Remove Test
+}
+
+const handleEditTest = async (record:any) => { 
+  //Edit Test
+}
+
+const TestTableColumns: ColumnsType<DataType> = [
+  {
+    key: 'sampleName',
+    title: 'Input sample',
+    dataIndex: 'sampleName',
+    render: (text) => <a>{text}</a>,
+    sorter: (a, b) => a.sampleName.length - b.sampleName.length,
+  },
+  {
+    key: 'testName',
+    title: 'Test Name',
+    dataIndex: 'sampleType',
+    render: (text) => <a>{text.testName}</a>,
+    sorter: (a, b) => a.sampleType.testName.length - b.sampleType.testName.length,
+  },
+  {
+    key: 'keywords',
+    title: 'Keywords (Hover to see aliases)',
+    dataIndex: 'sampleType',
+    sorter: (a, b) => a.sampleType.keywords.length - b.sampleType.keywords.length,
+    render: (sampleType, record) => (
+      <>
+        {sampleType.keywords?.map((param, index) => (
+          <a key={index} href='#'>
+            <Popover content={param.aliases} title={`${record?.sampleType?.testName} (${param.keyword} aliases)`}>
+              <Tag className="my-1" color="green" key={param}>
+                {param.keyword.toUpperCase()}
+              </Tag>
+            </Popover>
+          </a>
+        ))}
+      </>
+    ),
+  },
+  {
+    key: 'action',
+    title: 'Action',
+    dataIndex: 'action',
+    render: (text, record) => (
+      <Space size="middle">
+        <a>
+          <TrashIcon className='w-4 text-red-500' onClick={() => handleRemoveTest(record)} />
+        </a>
+        <a>
+          <PencilIcon onClick={() => handleEditTest(record)} className='w-4 text-gray-900' />
+        </a>
+      </Space>
+    ),
+  },
+];
 
 export const TestTable = () => {
-
-  const {user } = useUser()
-
   const [editTest,setEdit] = useState(false);
-  const [initialTestDetails,setInitalTest] = useState();
-  const [sampleName,setSampleName] = useState();
-  const [testName,setTestName] = useState();
-  const queryClient = useQueryClient();
   const profile = useProfileValue();
   const currentBranch = useCurrentBranchValue();
-  const operator = useManagerValue();
-  
-  const dispatch = useDispatch()
   const { confirm } = Modal;
-
-  const updateDiagnostic = useUpdateDiagnostic({
-    onSuccess: (data) => {
-      successAlert("Tests updated succesfully")
-      queryClient.invalidateQueries('getDiagnostic');
-    },
-    onError: (error) => {
-      successAlert("Error adding tests")
-    },
-  });
 
   //@ts-ignore
   let tests = profile?.tests.filter((test:any) => test?.branchId === currentBranch?._id)
 
-  let pathList = tests?.forEach((man:any) => {
-    return   { 
-     text: man.tastName, 
-     value: man.tastName 
-   };
-  });
-
-  const columns: ColumnsType<DataType> = [
-    {
-      key: 'sampleName',
-      title: 'Input sample',
-      dataIndex: 'sampleName',
-      render: (text) => <a>{text}</a>,
-      sorter: (a:any, b:any) => a.sampleName.length - b.sampleName.length,
-    },
-    {
-      key: 'testName',
-      title: 'Test Name',
-      dataIndex: 'sampleType',
-      render: (text) => <a>{text.testName}</a>,
-      sorter: (a:any, b:any) => a.testName.length - b.testName.length,
-      filters: pathList,
-      onFilter: (value: string, record) => record.testName.indexOf(value) === 0,
-    },
-    {
-      key: 'keywords',
-      title: 'Keywords (Hover to see aliases)',
-      dataIndex: 'sampleType',
-      sorter: (a:any, b:any) => a.keywords.length - b.keywords.length,
-      render: (sampleType,record) => (
-        <>
-          {sampleType.keywords && sampleType?.keywords.map((param,index) => {
-            return (
-                <a key={index} href='#'>
-                  <Popover content={(param.aliases)} title={record?.sampleType?.testName+" ("+param.keyword+" aliases)"}>
-                    <Tag className="my-1" color={"green"} key={param}>
-                      {param.keyword.toUpperCase()}
-                    </Tag>
-                  </Popover>
-                </a>
-            );
-          })}
-        </>
-      ),
-    },
-    {
-      key: 'action',
-      title: 'Action',
-      dataIndex: 'action',
-      render: (text:any,record:any,index:number) => (
-        <Space size="middle">
-         {<a > <TrashIcon className='w-4 text-red-500' onClick={()=>{
-           confirm({
-            title: 'Do you want to delete this test?',
-            content: 'The action cannot be undone.',
-            onOk() {
-                handleRemoveTest(record)}
-            }
-           )
-         }}/></a> }
-          <a ><PencilIcon onClick={()=>{handleEditTest(record)}} className='w-4 text-gray-900' /></a> 
-        </Space>
-      ),
-    }
-  ]
-
-  const handleRemoveTest = async (record:any) => {
-    let test = tests?.filter((test:any)=>test._id !== record._id)
-    updateDiagnostic?.mutate({data:{"tests":test},phoneNumber:user?.phoneNumbers[0]?.phoneNumber})
-    
-    // ActivityLogger("Removed Test "+record?.sampleName)
-  }
-
-  const handleEditTest = async (value:any) => {
-    dispatch({type:SET_TEST,payload:{}})
-    let initial = {
-      "testName": value?.sampleType?.testName,
-      "sampleName":value?.sampleName,
-      "keywords":value?.sampleType?.keywords,
-      "_id":value._id
-    }
-    let initial2 = {
-    
-      "sampleName":value?.sampleName,
-      "sampleType":{
-        "testName": value?.sampleType?.testName,
-        "keywords":value?.sampleType?.keywords,
-      }
-    }
-    dispatch({type:SET_TEST,payload:initial2})
-    setInitalTest(initial)
-    setEdit(!editTest)
-  }
-
-  const handleUpdateKeyword = async ()=> {
-  
-    let testItem = {
-      "sampleName": sampleName?.length>0? sampleName:initialTestDetails?.sampleName,
-      "sampleType": {
-        "testName": testName?.length>0 ?testName:initialTestDetails?.testName,
-        "keywords":testDetails?.sampleType?.keywords
-      },
-      "branchId" : activeBranch?._id
-    }
-
-    let updatedTest = profile?.tests.filter((test)=>test._id !== initialTestDetails?._id)
-    updatedTest.push(testItem)
-    updateDiagnostic?.mutate({data:{"tests":updatedTest},phoneNumber:user?.phoneNumbers[0]?.phoneNumber})
-    setEdit(!editTest)
-  }
-
   return (
-    <div>
-        {!editTest ? <DashboardTable pageSize={5} columns={columns} data={tests}/>:
-          <section className='p-2 sm:p-8 w-[100%] h-auto sm:max-h-[70vh] sm:overflow-y-scroll'>
-              <BodyStyled_2>Update Test Details</BodyStyled_2>
-              <section className='flex'>
-              <span className='w-[35%]'>
-                <p className='mt-10'>Custom Report Name</p>
-                <Input value={sampleName} onChange={(e)=>{setSampleName(e.target.value)}} defaultValue={initialTestDetails?.sampleName} name="sampleName" placeholder={"sampleName"} className="border-gray-300 w-[75%] mt-2 mb-10 rounded-lg text-black font-light text-sm py-2" />
-              </span>
-              <span className='w-[35%]'>
-                <p className='mt-10'>Test Name</p>
-                <Input value={testName} onChange={(e)=>{setTestName(e.target.value)}}  defaultValue={initialTestDetails?.testName} name="sampleName" placeholder={"sampleName"} className="border-gray-300 w-[75%] mt-2 mb-10 rounded-lg text-black font-light text-sm py-2" />
-              </span>
-              </section>
-              <AddKeywords edit={true} handleSucess={()=>{handleUpdateKeyword()}}  />
-          </section>
-        }
-    </div>
+      <div>
+          {!editTest ? <ViewTest columns={TestTableColumns} tests={tests}/> : <EditTests/>}
+      </div>
   )
-
 }
+
+const ViewTest = ({columns, tests}:any) => {
+  return <DashboardTable pageSize={5} columns={columns} data={tests}/>
+}
+
+const EditTests = () => {
+  return <section className='p-2 sm:p-8 w-[100%] h-auto sm:max-h-[70vh] sm:overflow-y-scroll'>
+    {/* <BodyStyled_2>Update Test Details</BodyStyled_2>
+    <section className='flex'>
+    <span className='w-[35%]'>
+      <p className='mt-10'>Custom Report Name</p>
+      <Input value={sampleName} onChange={(e)=>{setSampleName(e.target.value)}} defaultValue={initialTestDetails?.sampleName} name="sampleName" placeholder={"sampleName"} className="border-gray-300 w-[75%] mt-2 mb-10 rounded-lg text-black font-light text-sm py-2" />
+    </span>
+    <span className='w-[35%]'>
+      <p className='mt-10'>Test Name</p>
+      <Input value={testName} onChange={(e)=>{setTestName(e.target.value)}}  defaultValue={initialTestDetails?.testName} name="sampleName" placeholder={"sampleName"} className="border-gray-300 w-[75%] mt-2 mb-10 rounded-lg text-black font-light text-sm py-2" />
+    </span>
+    </section>
+    <AddKeywords edit={true} handleSucess={()=>{handleUpdateKeyword()}}  /> */}
+</section>
+}
+
+// export const TestTable = () => {
+
+//   const {user } = useUser()
+
+//   const [editTest,setEdit] = useState(false);
+//   const [initialTestDetails,setInitalTest] = useState();
+//   const [sampleName,setSampleName] = useState();
+//   const [testName,setTestName] = useState();
+//   const queryClient = useQueryClient();
+//   const profile = useProfileValue();
+//   const currentBranch = useCurrentBranchValue();
+//   const operator = useManagerValue();
+  
+//   const dispatch = useDispatch()
+//   const { confirm } = Modal;
+
+//   const updateDiagnostic = useUpdateDiagnostic({
+//     onSuccess: (data) => {
+//       successAlert("Tests updated succesfully")
+//       queryClient.invalidateQueries('getDiagnostic');
+//     },
+//     onError: (error) => {
+//       successAlert("Error adding tests")
+//     },
+//   });
+
+//   //@ts-ignore
+//   let tests = profile?.tests.filter((test:any) => test?.branchId === currentBranch?._id)
+
+//   let pathList = tests?.forEach((man:any) => {
+//     return   { 
+//      text: man.tastName, 
+//      value: man.tastName 
+//    };
+//   });
+
+//   const columns: ColumnsType<DataType> = [
+//     {
+//       key: 'sampleName',
+//       title: 'Input sample',
+//       dataIndex: 'sampleName',
+//       render: (text) => <a>{text}</a>,
+//       sorter: (a:any, b:any) => a.sampleName.length - b.sampleName.length,
+//     },
+//     {
+//       key: 'testName',
+//       title: 'Test Name',
+//       dataIndex: 'sampleType',
+//       render: (text) => <a>{text.testName}</a>,
+//       sorter: (a:any, b:any) => a.testName.length - b.testName.length,
+//       filters: pathList,
+//       onFilter: (value: string, record) => record.testName.indexOf(value) === 0,
+//     },
+//     {
+//       key: 'keywords',
+//       title: 'Keywords (Hover to see aliases)',
+//       dataIndex: 'sampleType',
+//       sorter: (a:any, b:any) => a.keywords.length - b.keywords.length,
+//       render: (sampleType,record) => (
+//         <>
+//           {sampleType.keywords && sampleType?.keywords.map((param,index) => {
+//             return (
+//                 <a key={index} href='#'>
+//                   <Popover content={(param.aliases)} title={record?.sampleType?.testName+" ("+param.keyword+" aliases)"}>
+//                     <Tag className="my-1" color={"green"} key={param}>
+//                       {param.keyword.toUpperCase()}
+//                     </Tag>
+//                   </Popover>
+//                 </a>
+//             );
+//           })}
+//         </>
+//       ),
+//     },
+//     {
+//       key: 'action',
+//       title: 'Action',
+//       dataIndex: 'action',
+//       render: (text:any,record:any,index:number) => (
+//         <Space size="middle">
+//          {<a > <TrashIcon className='w-4 text-red-500' onClick={()=>{
+//            confirm({
+//             title: 'Do you want to delete this test?',
+//             content: 'The action cannot be undone.',
+//             onOk() {
+//                 handleRemoveTest(record)}
+//             }
+//            )
+//          }}/></a> }
+//           <a ><PencilIcon onClick={()=>{handleEditTest(record)}} className='w-4 text-gray-900' /></a> 
+//         </Space>
+//       ),
+//     }
+//   ]
+
+//   const handleRemoveTest = async (record:any) => {
+//     let test = tests?.filter((test:any)=>test._id !== record._id)
+//     updateDiagnostic?.mutate({data:{"tests":test},phoneNumber:user?.phoneNumbers[0]?.phoneNumber})
+    
+//     // ActivityLogger("Removed Test "+record?.sampleName)
+//   }
+
+//   const handleEditTest = async (value:any) => {
+//     dispatch({type:SET_TEST,payload:{}})
+//     let initial = {
+//       "testName": value?.sampleType?.testName,
+//       "sampleName":value?.sampleName,
+//       "keywords":value?.sampleType?.keywords,
+//       "_id":value._id
+//     }
+//     let initial2 = {
+    
+//       "sampleName":value?.sampleName,
+//       "sampleType":{
+//         "testName": value?.sampleType?.testName,
+//         "keywords":value?.sampleType?.keywords,
+//       }
+//     }
+//     dispatch({type:SET_TEST,payload:initial2})
+//     setInitalTest(initial)
+//     setEdit(!editTest)
+//   }
+
+//   const handleUpdateKeyword = async ()=> {
+  
+//     let testItem = {
+//       "sampleName": sampleName?.length>0? sampleName:initialTestDetails?.sampleName,
+//       "sampleType": {
+//         "testName": testName?.length>0 ?testName:initialTestDetails?.testName,
+//         "keywords":testDetails?.sampleType?.keywords
+//       },
+//       "branchId" : activeBranch?._id
+//     }
+
+//     let updatedTest = profile?.tests.filter((test)=>test._id !== initialTestDetails?._id)
+//     updatedTest.push(testItem)
+//     updateDiagnostic?.mutate({data:{"tests":updatedTest},phoneNumber:user?.phoneNumbers[0]?.phoneNumber})
+//     setEdit(!editTest)
+//   }
+
+//   return (
+//     <div>
+//         {!editTest ? <DashboardTable pageSize={5} columns={columns} data={tests}/>:
+//           <section className='p-2 sm:p-8 w-[100%] h-auto sm:max-h-[70vh] sm:overflow-y-scroll'>
+//               <BodyStyled_2>Update Test Details</BodyStyled_2>
+//               <section className='flex'>
+//               <span className='w-[35%]'>
+//                 <p className='mt-10'>Custom Report Name</p>
+//                 <Input value={sampleName} onChange={(e)=>{setSampleName(e.target.value)}} defaultValue={initialTestDetails?.sampleName} name="sampleName" placeholder={"sampleName"} className="border-gray-300 w-[75%] mt-2 mb-10 rounded-lg text-black font-light text-sm py-2" />
+//               </span>
+//               <span className='w-[35%]'>
+//                 <p className='mt-10'>Test Name</p>
+//                 <Input value={testName} onChange={(e)=>{setTestName(e.target.value)}}  defaultValue={initialTestDetails?.testName} name="sampleName" placeholder={"sampleName"} className="border-gray-300 w-[75%] mt-2 mb-10 rounded-lg text-black font-light text-sm py-2" />
+//               </span>
+//               </section>
+//               <AddKeywords edit={true} handleSucess={()=>{handleUpdateKeyword()}}  />
+//           </section>
+//         }
+//     </div>
+//   )
+
+// }
--- components/organism/dashboardTabs/testTab.tsx
+++ components/organism/dashboardTabs/testTab.tsx
@@ -1,18 +1,17 @@
-
+import React, { useState } from 'react';
 import { TestTable } from '@components/molecules/test/testTable';
 import { TestToggle } from '@components/molecules/test/testToggle';
-import React, { useState } from 'react'
 import { AddTestComponent } from '@components/molecules/addReport/addTest';
 
 export function TestTab() {
-  const [showTest,setShowTest] = useState(false)
+  const [showTest, setShowTest] = useState(false);
 
   return (
-      <div className="p-0 sm:p-6 xl:p-8 h-auto bg-signBanner relative flex justify-center">
-            <TestToggle showTest={showTest} setShowTest={setShowTest}  />
-            <div className='w-[95vw] md:w-[90vw] xl:w-[70vw] h-auto bg-white shadow-2xl sm:shadow-lg my-24 sm:mt-14  rounded-lg'> 
-            {!showTest ? <TestTable /> : <AddTestComponent setTest={setShowTest} />}
-            </div>
+    <div className="p-0 sm:p-6 xl:p-8 h-auto bg-signBanner relative flex justify-center">
+      <TestToggle showTest={showTest} setShowTest={setShowTest} />
+      <div className="w-[95vw] md:w-[90vw] xl:w-[70vw] h-auto bg-white shadow-2xl sm:shadow-lg my-24 sm:mt-14 rounded-lg">
+        {!showTest ? <TestTable /> : <AddTestComponent setTest={setShowTest} />}
       </div>
-  )
+    </div>
+  );
 }
