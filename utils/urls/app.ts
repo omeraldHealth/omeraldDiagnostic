4f7699b3ad614bf3f42ec788912636fc3e9ea817
msaif001 <saifmohammed888@gmail.com>
2024-04-28 18:12:24 +0530

    fix: url update in env

--- utils/urls/app.ts
+++ utils/urls/app.ts
@@ -1,27 +1,21 @@
 // Base URL for the backend API
-// export const backendApiPath = "https://diagnostic-backend.onrender.com/";
-export const backendApiPath = "http://localhost:3002/";
+export const diagBackendApiPath = process.env.NEXT_PUBLIC_DIAG_BASE_API;
+export const adminBackendApiPath = process.env.NEXT_PUBLIC_ADMIN_BASE_API;
 
 // API paths for diagnostic profiles
-export const getDiagProfileByPhoneApi = backendApiPath + `profiles/getProfile/`; // Endpoint to get diagnostic profile by phone
-export const createDiagProfileApi = backendApiPath + `profiles/createProfile/`; // Endpoint to create diagnostic profile
-export const updateDiagProfileApi = backendApiPath + `profiles/updateProfile`; // Endpoint to update diagnostic profile
+export const getDiagProfileByPhoneApi = diagBackendApiPath + `profiles/getProfile/`; // Endpoint to get diagnostic profile by phone
+export const createDiagProfileApi = diagBackendApiPath + `profiles/createProfile/`; // Endpoint to create diagnostic profile
+export const updateDiagProfileApi = diagBackendApiPath + `profiles/updateProfile`; // Endpoint to update diagnostic profile
 
 // API paths for diagnostic reports
-export const getDiagReportsApi = backendApiPath + `reports/getReports/`; // Endpoint to get diagnostic reports
-export const createDiagReportsApi = backendApiPath + `reports/createReport/`; // Endpoint to create diagnostic report
-export const deleteDiagReportsApi = backendApiPath + `reports/deleteReport/`; // Endpoint to create diagnostic report
+export const getDiagReportsApi = diagBackendApiPath + `reports/getReports/`; // Endpoint to get diagnostic reports
+export const createDiagReportsApi = diagBackendApiPath + `reports/createReport/`; // Endpoint to create diagnostic report
+export const deleteDiagReportsApi = diagBackendApiPath + `reports/deleteReport/`; // Endpoint to create diagnostic report
 
-
-export const uploadDiagnosticReportApi = backendApiPath + `s3/omerald/logoUpload`; // Endpoint to upload diagnostic report
-export const uploadDiagnosticLogoApi = backendApiPath + `s3/diagnostic/reportUpload`; // Endpoint to upload diagnostic logo
+export const uploadDiagnosticReportApi = diagBackendApiPath + `s3/omerald/logoUpload`; // Endpoint to upload diagnostic report
+export const uploadDiagnosticLogoApi = diagBackendApiPath + `s3/diagnostic/reportUpload`; // Endpoint to upload diagnostic logo
 
 // External API path to get all admin report types
-export const getAdminReportTypesApi = "https://adminstage.azurewebsites.net/api/v1/reports/getAllReports";
-// export const getDiagnosticLandingData = "https://adminstage.azurewebsites.net/api/v1/diagnosticLanding/getDiagLanding"
-export const getDiagnosticLandingData = "http://localhost:3001/api/v1/diagnosticLanding/getDiagLanding";
-
-
-export const getAdminApiGetToken = "https://adminstage.azurewebsites.net/api/v1/auth/getAuthToken"
-
-// Comments added to explain the purpose of each API endpoint
+export const getAdminReportTypesApi = adminBackendApiPath+"/api/v1/reports/getAllReports";
+export const getDiagnosticLandingData = adminBackendApiPath+"/api/v1/diagnosticLanding/getDiagLanding"
+export const getAdminApiGetToken = adminBackendApiPath+"/api/v1/auth/getAuthToken"
