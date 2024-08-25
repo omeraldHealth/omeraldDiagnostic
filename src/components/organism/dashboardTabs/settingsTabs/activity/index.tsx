867d6fce4148b72e2a85330a64922197c9c86082
msaif001 <saifmohammed888@gmail.com>
2024-08-26 00:56:11 +0530

    fix: path

--- src/components/organism/dashboardTabs/settingsTabs/activity/index.tsx
+++ src/components/organism/dashboardTabs/settingsTabs/activity/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { CommonSettingTable } from "../utils/table";
 import { ADMIN_USER_ACTIVITES_COLUMNS } from "../utils/tabs";
-import { useCurrentBranchValue } from "@components/common/constants/recoilValues";
+import { useCurrentBranchValue } from "../../../../common/constants/recoilValues";
 
 export const Activity = () => {
   const currentBranch = useCurrentBranchValue();
